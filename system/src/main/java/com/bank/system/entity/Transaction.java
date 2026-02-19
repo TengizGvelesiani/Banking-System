9a40bd06566f0a081198f0fe47c0e5e51ceb13ac
Tengiz <tgvel22@freeuni.edu.ge>
2026-02-19 19:10:32 +0400

    Implement Account and Transaction Entity

--- system/src/main/java/com/bank/system/entity/Transaction.java
+++ system/src/main/java/com/bank/system/entity/Transaction.java
@@ -1,8 +1,63 @@
 package com.bank.system.entity;
 
+import java.math.BigDecimal;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.transaction.TransactionStatus;
+
+import com.bank.system.enums.TransactionType;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "transactions")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Transaction {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_account_id")
+    private String senderAccount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipient_account_id")
+    private String recipientAccount;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(length = 150)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionStatus status;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private java.time.LocalDateTime timestamp;
 }
